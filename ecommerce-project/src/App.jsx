8f1cba7a4782a2b768b12e2c8e68072440f82084
Vickymaurya1 <vickymaurya2005@gmail.com>
2026-01-12 21:39:41 +0530

    learning router and routes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,4 @@
+import {Routes , Route} from 'react-router';
 import {HomePage} from './pages/HomePage';
 
 import './App.css'
@@ -5,7 +6,13 @@ import './App.css'
 function App() {
  
   return (
-  <HomePage/>
+
+    <Routes>
+      {/* <Route path = "/" element = {<HomePage/>}></Route> */}
+      <Route path = "/" element = {<HomePage/>}></Route>
+      <Route path = "checkout" element = {}> </Route>
+    </Routes>
+
   )
 }
 
